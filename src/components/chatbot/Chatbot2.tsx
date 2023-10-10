3bfcbfe32c4794cbfdc27dfbdcc2b7f2f664c584
jnk <jnk626@pm.me>
2023-10-10 15:17:13 +0200

    implements (again) recognition

--- src/components/chatbot/Chatbot2.tsx
+++ src/components/chatbot/Chatbot2.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 const Dictaphone = () => {
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition
-  } = useSpeechRecognition();
+  const commands = [
+    {
+      command: "hello",
+      callback: () => setMessage("Hello there!"),
+    }
+  ]
+  const { transcript, resetTranscript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition({ commands });
+  const [isListening, setIsListening] = useState(false);
+  const [message, setMessage] = useState("");
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
+  const handleListening = () => {
+    setIsListening(true);
+    SpeechRecognition.startListening({ continuous: true, language: "en-EN"});
+  };
+
+  const stopListening = () => {
+    setIsListening(false);
+    SpeechRecognition.stopListening();
+  };
+
+  const handleReset = () => {
+    stopListening();
+    resetTranscript();
+  };
+
   return (
     <div>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onClick={() => SpeechRecognition.startListening()}>Start</button>
-      <button onClick={() => SpeechRecognition.stopListening()}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+      <p>Microphone: {isListening ? "on" : "off"}</p>
+      <button onClick={handleListening}>Start</button>
+      {isListening && <button onClick={stopListening}>Stop</button>}
+      {transcript && (
+        <div>
+          <p>{message}</p>
+          <br />
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      )}
     </div>
   );
 };
