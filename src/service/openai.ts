eb8ff6551fd71535cdf0d3fb4092e7b959062d18
jnk <jnk626@pm.me>
2023-10-10 17:03:20 +0200

    starts setting up openai

---
+++ src/service/openai.ts
@@ -0,0 +1,8 @@
+import axios from "axios"
+
+const instance = axios.create({
+    baseURL: "",
+    headers: {
+        Authorization: `Bearer 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`
+    }
+})
